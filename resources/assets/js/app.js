22a150dbc9870213c911d21afcadde5a6e047126
Džiugas Butkus <dziubutkus@gmail.com>
2017-08-04 15:34:19 -0600

    nav

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,13 +13,14 @@ import Buefy from 'buefy';
 
 Vue.use(Buefy);
 
+var app = new Vue({
+    el: '#app',
+    data: {}
+})
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// Vue.component('example', require('./components/Example.vue'));
-
+$(document).ready(function(){
+    $('button.dropdown').hover(function(e) {
+        $(this).toggleClass('is-open');
+    });
+});
 
